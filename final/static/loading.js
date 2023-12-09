bee6ecd4a09bf5c3102d9de150d3f32ec022320d
Sam Khodak <khod2@pdx.edu>
2023-12-09 05:06:21 -0800

    added loading icon on form submissions.

---
+++ final/static/loading.js
@@ -0,0 +1,7 @@
+// This file is for any javascript functions that need to be ran in loading a page.
+
+
+function showLoadingIcon() {
+    loading_icon = document.getElementById("loading-blur")
+    loading_icon.style.display = "block"
+}
